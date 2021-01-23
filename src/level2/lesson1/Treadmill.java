e4a9f5595446a85c99899028a25127d8cfe5d2c7
Viktor <viktor.kiselev1992@gmail.com>
2021-01-23 17:31:36 +0500

    Level 2 lesson 1

--- src/level2/lesson1/Treadmill.java
+++ src/level2/lesson1/Treadmill.java
@@ -1,18 +1,5 @@
 package level2.lesson1;
 
 public class Treadmill {
-    public static final int MAX_LENGHT = 1000;
-    private int lenght;
 
-    public Treadmill(int hight) {
-        this.lenght = hight;
-    }
-
-    public Treadmill() {
-        this.lenght = (int) (Math.random()*MAX_LENGHT);
-    }
-
-    public int getLenght() {
-        return lenght;
-    }
 }
