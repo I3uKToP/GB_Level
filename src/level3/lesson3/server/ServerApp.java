122d78d824517eb64369927a933950062465fa81
Viktor <viktor.kiselev1992@gmail.com>
2021-03-11 21:27:13 +0500

    level 3 lesson3

---
+++ src/level3/lesson3/server/ServerApp.java
@@ -0,0 +1,9 @@
+package level3.lesson3.server;
+
+import level3.lesson3.server.service.MyServer;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        new MyServer();
+    }
+}
