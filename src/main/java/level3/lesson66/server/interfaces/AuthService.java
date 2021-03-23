6b781596fef3eb9ebad523d7d9af45f6104cb6e1
I3uKToP <73830197+I3uKToP@users.noreply.github.com>
2021-03-23 23:23:30 +0500

    Delete src/main/java/level3/lesson66 directory

--- src/main/java/level3/lesson66/server/interfaces/AuthService.java
+++
@@ -1,9 +0,0 @@
-package level3.lesson66.server.interfaces;
-
-
-public interface AuthService {
-    void start();
-    void stop();
-    String getNickByLoginAndPassword(String login, String password);
-}
-
